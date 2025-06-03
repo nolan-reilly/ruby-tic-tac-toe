957aaf95a49a161a8395659dad351edf82cb543a
Nolan Reilly <nolanreilly02@gmail.com>
2025-06-03 11:18:52 -0500

    Create initial project setup

---
+++ lib/board.rb
@@ -0,0 +1,5 @@
+class Board
+  def initialize(height, width)
+    @board = []
+  end
+end
---
+++ main.rb
@@ -0,0 +1,21 @@
+# Notes
+# -------------------------------------
+# - Game Controller
+#   - player_won?
+#   - switch turns
+#   - board logic detecting who won
+#   - Displaying the board
+#   - Prompting players for info 
+#
+# - Board Class
+#   - Height
+#   - Width
+#   - Contains Cells
+#   
+# - Cell Class
+#   - value
+#   
+# - Player Class
+#   - Name
+#   - is_turn
+#   - value
